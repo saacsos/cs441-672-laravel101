7e93895d0d47a8ab28c47998abb714bfebaf155b
Saac Sornchai <sornchai.la@ku.th>
2025-02-26 17:22:07 +0700

    api

---
+++ routes/api.php
@@ -0,0 +1,30 @@
+<?php
+
+use App\Http\Controllers\API\ArtistController;
+use App\Http\Controllers\API\Auth\AuthenticateController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+Route::middleware('throttle:api')->as('api.')->group(function () {
+    Route::get('/', function () {
+        return [
+            'success' => true,
+            'version' => '1.0.0',
+        ];
+    });
+    Route::apiResource('artists', ArtistController::class);
+    Route::post('login', [AuthenticateController::class, 'login'])->name('user.login');
+});
+
+Route::middleware(['throttle:api', 'auth:sanctum'])->as('api.')->group(function () {
+
+    Route::delete('revoke', [AuthenticateController::class, 'revoke'])->name('user.revoke');
+});
+
+//Route::middleware('throttle:member')->group(function () {
+//
+//});
